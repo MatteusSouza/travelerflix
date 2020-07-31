92ab9bafbafa6e5064f92c35524d6ba145faa066
MatteusSouza <61070878+MatteusSouza@users.noreply.github.com>
2020-07-30 22:35:44 -0300

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FooterBase } from './styles';
+import MyGitOctocat from '../../assets/img/MyGitOctocat.png'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://github.com/MatteusSouza">Github</a>
+      {' / '}
+      <a href="https://www.linkedin.com/in/matteus-souza/">Linkedin</a>
+      <p>
+      Projeto feito em React durante o bootcamp da Alura.
+        <span role="img" aria-label="sheep">💙💙</span>
+      </p>
+        <a href="https://github.com/MatteusSouza">
+        <img className="MyGitOctocat" src={MyGitOctocat} alt="MyGitOctocat" />
+      </a>
+    </FooterBase>
+  );
+}
+
+export default Footer;
