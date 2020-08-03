d82531f73e9ce69d84f0a56b921b35da83af705f
MatteusSouza <61070878+MatteusSouza@users.noreply.github.com>
2020-08-02 22:04:25 -0300

    Added a form page and a fake rest API

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,20 +1,124 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-    return (
-        <PageDefault>
-            <h1>Página de Cadastro de Categoria</h1>
+  const valoresIniciais = {
+    nome: '',
+    descrição: '',
+    cor: '',
+  };
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
+  function setValue(chave, valor) {
+    // chave: nome, descrição ...
+    setValues({
+      ...values,
+      [chave]: valor, // nome: 'valor'
+    });
+  }
 
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
+    );
+  }
 
+  useEffect(() => {
+    console.log('useEffects ');
+    const URL_SERVER = 'http://localhost:8080/categorias';
+    fetch(URL_SERVER)
+      .then(async (respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+    // setTimeout(() => {
+    //   setCategorias([
+    //     ...categorias,
+    //     {
+    //       id: 1,
+    //       nome: 'Noruega',
+    //       descrição: 'Canais sobrea a Noruega',
+    //       cor: '#085AC4',
+    //     },
+    //     {
+    //       id: 2,
+    //       nome: 'Brasil',
+    //       descrição: 'O melhor do Brasil',
+    //       cor: '#085AC4',
+    //     },
+    //   ]);
+    // }, 4 * 1000);
+  }, []);
 
-            <Link to="/">
-                Ir para home
-            </Link>
-        </PageDefault>
-    )
-};
+  return (
+    <PageDefault>
+      <h1>
+        Página de Cadastro de Categoria:
+        {values.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
+        setValues(valoresIniciais);
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descrição"
+          value={values.descrição}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      {categorias.length === 0 && (
+        <div>
+          Carregando...
+        </div>
+      )}
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.name}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
+}
 
 export default CadastroCategoria;
